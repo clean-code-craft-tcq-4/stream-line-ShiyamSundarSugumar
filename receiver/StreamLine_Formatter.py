1f68f1c1a60b9bf57154ebf4ef80bf153f5363ef
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-10-31 12:41:40 +0530

    Update StreamLine_Formatter.py

--- receiver/StreamLine_Formatter.py
+++ receiver/StreamLine_Formatter.py
@@ -6,17 +6,19 @@ def StreamLineformat_console_pipeline():
     data = sys.stdin.readlines()
     input_data = ''
     
-    #get the streaming data from dotnet console output in the pipeline
-    for line in data:
-        if 'temperature' in line:
-            input_data = line
-            
-    #convert StreamLine data string to list of dictionaries
-    formated_data_stream = ast.literal_eval(input_data)
+    if(data != []):
     
-    #convert the list of dictionaries to a dataframe and return bms parameter lists
-    StreamLine_dataframe = pd.DataFrame(formated_data_stream)
-    if StreamLine_dataframe.size !=0:
-        StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
-        StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
-        return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
+        #get the streaming data from dotnet console output in the pipeline
+        for line in data:
+            if 'temperature' in line:
+                input_data = line
+
+        #convert StreamLine data string to list of dictionaries
+        formated_data_stream = ast.literal_eval(input_data)
+
+        #convert the list of dictionaries to a dataframe and return bms parameter lists
+        StreamLine_dataframe = pd.DataFrame(formated_data_stream)
+        if StreamLine_dataframe.size !=0:
+            StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
+            StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
+            return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
