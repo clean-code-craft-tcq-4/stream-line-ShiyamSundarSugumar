b0d7c0c18d15d95b99e2da2b2051a491083ed021
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-10-31 12:45:04 +0530

    Update StreamLine_Reporter.py

--- receiver/StreamLine_Reporter.py
+++ receiver/StreamLine_Reporter.py
@@ -13,16 +13,17 @@ def StreamLineReceiver_max(StreamLine_data):
 
 # report the statistics of StreamLine data received from sender
 def displayOutput(StreamLine_data):
-    StreamLineReceiver_temperature_list = StreamLine_data[0]
-    StreamLineReceiver_soc_list = StreamLine_data[1]
+    if(StreamLine_data!=None):
+        StreamLineReceiver_temperature_list = StreamLine_data[0]
+        StreamLineReceiver_soc_list = StreamLine_data[1]
 
-    print('Max Temperature:', StreamLineReceiver_max(StreamLineReceiver_temperature_list))
-    print('Min Temperature:', StreamLineReceiver_min(StreamLineReceiver_temperature_list))
-    print('Moving average of last five values of temperature:', StreamLineReceiver_moving_average_last_5(StreamLineReceiver_temperature_list))
+        print('Max Temperature:', StreamLineReceiver_max(StreamLineReceiver_temperature_list))
+        print('Min Temperature:', StreamLineReceiver_min(StreamLineReceiver_temperature_list))
+        print('Moving average of last five values of temperature:', StreamLineReceiver_moving_average_last_5(StreamLineReceiver_temperature_list))
 
-    print('Max SOC:', StreamLineReceiver_max(StreamLineReceiver_soc_list))
-    print('Min SOC:', StreamLineReceiver_min(StreamLineReceiver_soc_list))
-    print('Moving average of last five values of chargeRate:', StreamLineReceiver_moving_average_last_5(StreamLineReceiver_soc_list))
+        print('Max SOC:', StreamLineReceiver_max(StreamLineReceiver_soc_list))
+        print('Min SOC:', StreamLineReceiver_min(StreamLineReceiver_soc_list))
+        print('Moving average of last five values of chargeRate:', StreamLineReceiver_moving_average_last_5(StreamLineReceiver_soc_list))
 
 # returns moving average of last 5 values in bms parameter list
 def StreamLineReceiver_moving_average_last_5(StreamLine_data):
