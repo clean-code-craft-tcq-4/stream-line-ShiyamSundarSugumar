d45506aadd855bde212219f1789a40a1919b9e76
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-11-03 18:35:52 +0530

    Update StreamLine_Formatter.py

--- receiver/StreamLine_Formatter.py
+++ receiver/StreamLine_Formatter.py
@@ -1,20 +1,23 @@
 import sys
 import pandas as pd
 
-def StreamLineformat_console_pipeline():
+def Read_from_console():
     i=0
     data_1 = []
     while(i<77):
         data_1.append(input())
         i+=1
-
     print(data_1)   
     data_2 = data_1[27:77]
+    return data_2
+    
+
+def StreamLineformat_console_pipeline(data):
     i=0
     StreamLineReceiver_temperature_list = []
     StreamLineReceiver_soc_list = []
     while(i<50):
-        temp = data_2[i].split(",")
+        temp = data[i].split(",")
         StreamLineReceiver_temperature_list.append(int(temp[0]))
         StreamLineReceiver_soc_list.append(int(temp[1]))
         i+=1    
