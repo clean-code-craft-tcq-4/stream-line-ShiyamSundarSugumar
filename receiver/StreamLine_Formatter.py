4a4abbc013b2a84af5630ea86249645999962c20
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-11-03 15:49:42 +0530

    Update StreamLine_Formatter.py

--- receiver/StreamLine_Formatter.py
+++ receiver/StreamLine_Formatter.py
@@ -1,38 +1,52 @@
 import sys
 import ast
 import pandas as pd
+import re
 
-def StreamLineformat_console_pipeline():
-    data = sys.stdin.readlines()
-    input_data = ''
-    print(data)
-    if(data != []):
-        def findIndex(data):
-            dataIndexOfTemperature = data.index('[Temperature,SOC]\n')
-            print(dataIndexOfTemperature)
-        IndexOfTemperature = findIndex(data)
-        
-        def filterFunc(data):
-            if(data == '[Temperature,SOC]\n'):
-                return True
-            else:
-                return False
-        Streamdata = filter(filterFunc, data)
-        for x in Streamdata:
-            print(x)
-        #get the streaming data from dotnet console output in the pipeline
-        for line in data:
-            if '[Temperature,SOC]\n' in line:
-                input_data = line
+class dataFormatter
+    def dataReadFromconsole(self, data):
+        data = sys.stdin.readlines()
+        input_data = ''
+        print(data)
+        return data
+    consoledata = dataReadFromconsole(data)
+    
+    def dataformatter(self, formattedData):
+        formattedData = [re.sub('[^a-zA-Z0-9]', "", _) for _ in formattedData]
+        print(formattedData)   
+        return formattedData
 
-        #convert StreamLine data string to list of dictionaries
-        formated_data_stream = ast.literal_eval(input_data)
+    def StreamLineformat_console_pipeline():
+        data = sys.stdin.readlines()
+        input_data = ''
+        print(data)
+        if(data != []):
+            def findIndex(data):
+                dataIndexOfTemperature = data.index('[Temperature,SOC]\n')
+                print(dataIndexOfTemperature)
+            IndexOfTemperature = findIndex(data)
 
-        #convert the list of dictionaries to a dataframe and return bms parameter lists
-        StreamLine_dataframe = pd.DataFrame(formated_data_stream)
-        if StreamLine_dataframe.size !=0:
-            StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
-            StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
-            return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
-    else:
-        return None
+            def filterFunc(data):
+                if(data == '[Temperature,SOC]\n'):
+                    return True
+                else:
+                    return False
+            Streamdata = filter(filterFunc, data)
+            for x in Streamdata:
+                print(x)
+            #get the streaming data from dotnet console output in the pipeline
+            for line in data:
+                if '[Temperature,SOC]\n' in line:
+                    input_data = line
+
+            #convert StreamLine data string to list of dictionaries
+            formated_data_stream = ast.literal_eval(input_data)
+
+            #convert the list of dictionaries to a dataframe and return bms parameter lists
+            StreamLine_dataframe = pd.DataFrame(formated_data_stream)
+            if StreamLine_dataframe.size !=0:
+                StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
+                StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
+                return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
+        else:
+            return None
