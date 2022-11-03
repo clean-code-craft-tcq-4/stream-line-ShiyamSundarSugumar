467eff212f031f3e1fd72c21de825daecead1e4a
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-11-03 18:39:07 +0530

    Update test_StreamLine_Receiver.py

--- receiver/test_StreamLine_Receiver.py
+++ receiver/test_StreamLine_Receiver.py
@@ -32,6 +32,7 @@ class test_StreamLine_receiver(unittest.TestCase):
         
     # test if StreamLineReceiver statistics report is printed successfully      
     def test_StreamLineReceiver_statistics_reporting(self):
+        data = ["65,25","55,25"]
         self.assertTrue(StreamLine_Reporter.displayOutput(StreamLineformat_console_pipeline()) == 'Success')
         
 if __name__ == '__main__':
