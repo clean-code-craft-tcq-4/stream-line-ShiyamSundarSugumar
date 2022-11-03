ad2a8cccfdfc347ffaeee755d1f327aed02590dc
VaniKayam <105203875+VaniKayam@users.noreply.github.com>
2022-11-03 17:01:07 +0530

    Update StreamLine_Formatter.py

--- receiver/StreamLine_Formatter.py
+++ receiver/StreamLine_Formatter.py
@@ -3,57 +3,44 @@ import ast
 import pandas as pd
 import re
 
-class dataFormatter
-    def dataReadFromconsole(self, data):
-        data = sys.stdin.readlines()
-        input_data = ''
-        print(data)
-        return data
-    consoledata = dataReadFromconsole(data)
-    
-    def dataformatter(self, formattedData):
-        formattedData = [re.sub('[^a-zA-Z0-9]', "", _) for _ in formattedData]
-        print(formattedData)   
-        return formattedData
+def StreamLineformat_console_pipeline():
+    i=0
+    data_1 = " "
+    while(i<50):
+        data_1.append(input())
+        i+=1
 
-    def StreamLineformat_console_pipeline():
-        i=0
-        data_1 = " "
-        while(i<50):
-            data_1.append(input())
-            i+=1
-            
-        print(data_1)
-        data = sys.stdin.readlines()
-        input_data = ''
-        print(data)
-        if(data != []):
-            def findIndex(data):
-                dataIndexOfTemperature = data.index('[Temperature,SOC]\n')
-                print(dataIndexOfTemperature)
-            IndexOfTemperature = findIndex(data)
+    print(data_1)
+    data = sys.stdin.readlines()
+    input_data = ''
+    print(data)
+    if(data != []):
+        def findIndex(data):
+            dataIndexOfTemperature = data.index('[Temperature,SOC]\n')
+            print(dataIndexOfTemperature)
+        IndexOfTemperature = findIndex(data)
 
-            def filterFunc(data):
-                if(data == '[Temperature,SOC]\n'):
-                    return True
-                else:
-                    return False
-            Streamdata = filter(filterFunc, data)
-            for x in Streamdata:
-                print(x)
-            #get the streaming data from dotnet console output in the pipeline
-            for line in data:
-                if '[Temperature,SOC]\n' in line:
-                    input_data = line
+        def filterFunc(data):
+            if(data == '[Temperature,SOC]\n'):
+                return True
+            else:
+                return False
+        Streamdata = filter(filterFunc, data)
+        for x in Streamdata:
+            print(x)
+        #get the streaming data from dotnet console output in the pipeline
+        for line in data:
+            if '[Temperature,SOC]\n' in line:
+                input_data = line
 
-            #convert StreamLine data string to list of dictionaries
-            formated_data_stream = ast.literal_eval(input_data)
+        #convert StreamLine data string to list of dictionaries
+        formated_data_stream = ast.literal_eval(input_data)
 
-            #convert the list of dictionaries to a dataframe and return bms parameter lists
-            StreamLine_dataframe = pd.DataFrame(formated_data_stream)
-            if StreamLine_dataframe.size !=0:
-                StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
-                StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
-                return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
-        else:
-            return None
+        #convert the list of dictionaries to a dataframe and return bms parameter lists
+        StreamLine_dataframe = pd.DataFrame(formated_data_stream)
+        if StreamLine_dataframe.size !=0:
+            StreamLineReceiver_temperature_list = StreamLine_dataframe['temperature'].values.tolist()
+            StreamLineReceiver_soc_list = StreamLine_dataframe['soc'].values.tolist()
+            return [StreamLineReceiver_temperature_list,StreamLineReceiver_soc_list]
+    else:
+        return None
